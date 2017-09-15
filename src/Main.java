f0a9e193511e2fcf640c9e78de5b001314d3f96e
pilmyday <roy00374@gmail.com>
2017-09-15 15:05:49 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+	  System.out.println("學號:106022067");
+	  System.out.println("系級:行動商務與多媒體應用一年級");
+	  System.out.println("姓名:楊秉憲");
+	  System.out.println("時間:2017/9/15");
+	  System.out.println("地點:I432");
+    }
+}
